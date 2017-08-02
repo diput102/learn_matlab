78e195105ef3f88b8752618d4628f4ab15e46707
diput102 <2382634989@qq.com>
2017-08-02 23:39:42 +0800

    delete ganma func

--- 8_1/ex3_1.m
+++
@@ -1,8 +0,0 @@
-I=imread('pout.tif');
-figure;
-imshow(I);
-title('Source');
-figure;
-imhist(I,1024);
-title('Graph');
-figure,imhist(I);
--- 8_1/ex3_2.m
+++
@@ -1,6 +0,0 @@
-I=imread('pout.tif');
-figure;
-[M,N]=size(I);
-[counts,x]=imhist(I,64);
-counts=counts/M/N;
-stem(x,counts);
